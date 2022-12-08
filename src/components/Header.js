b3d98722071a4a8bc712e5934c4dae61a2be1a96
matshel <mats.helgesen@protonmail.com>
2022-12-08 04:30:30 +0100

    some coding

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// import sidebar context
+import { SidebarContext } from "../contexts/SidebarContext";
+
+// import react icons
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  return (
+    <header className='bg-blue-200'>
+      <div>Header</div>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className='relative flex cursor-pointer'
+      >
+        <BsBag className='text-2xl' />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// import Link
+import { Link } from "react-router-dom";
+
+// import react icons
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import { FiTrash2 } from "react-icons/fi";
+
+// import components
+import CartItem from "../components/CartItem";
+
+// import sidebar context
+import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isOpen, handleClose } = useContext(SidebarContext);
+  return (
+    <div
+      className={`${
+        isOpen ? "right-0" : "-right-full"
+      }  fixed top-0 w-full bg-white  h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    >
+      <div className='flex justify-between items-center py-6 border-b '>
+        <div className='uppercase text-sm font-semibold '>Shopping Bag (0)</div>
+        {/* icon */}
+        <div
+          onClick={handleClose}
+          className='flex justify-center items-center cursor-pointer w-8 h-8'
+        >
+          <AiOutlineCloseSquare className='text-2xl' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
