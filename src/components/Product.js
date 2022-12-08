91604fe3b893b01612847b280b8f93628251c5fe
matshel <mats.helgesen@protonmail.com>
2022-12-08 02:09:38 +0100

    coded

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,49 @@
 import React from "react";
 
-const Product = () => {
-  return <div>Product</div>;
+// import Link
+import { Link } from "react-router-dom";
+
+// import react icons
+import { BsPlus, BsEyeFill } from "react-icons/bs";
+
+const Product = ({ product }) => {
+  // destructure product data
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className='relative border border-[#e4e4e4] h-[300px] mb-4 overflow-hidden group transition'>
+        <div className='w-full h-full flex justify-center items-center'>
+          <div className='flex justify-center items-center mx-auto w-[200px]'>
+            <img
+              className='max-h-[160px] group-hover:scale-110 transition duration-300'
+              src={image}
+              alt={title}
+            />
+          </div>
+        </div>
+        {/* buttons */}
+        <div className='absolute top-0 -right-11 group-hover:right-0 flex flex-col justify-center items-center gap-y-2 p-2  opacity-0 group-hover:opacity-100 transition-all'>
+          <button>
+            <div className='flex justify-center items-center text-white  w-12 h-12 bg-red-500'>
+              <BsPlus className='text-3xl ' />
+            </div>
+          </button>
+          <Link
+            to={`/product/${id}`}
+            className='flex justify-center items-center w-12 h-12 bg-white text-primary drop-shadow-xl'
+          >
+            <BsEyeFill />
+          </Link>
+        </div>
+      </div>
+      {/* category & title & price */}
+      <div className='text-sm capitalize text-gray-500 mb-1'> {category} </div>
+      <Link to={`/product/${id}`}>
+        <h2 className='font-semibold mb-1'> {title} </h2>
+      </Link>
+      <div className='font-semibold'>${price} </div>
+    </div>
+  );
 };
 
 export default Product;
